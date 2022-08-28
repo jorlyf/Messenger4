c51f65e7b7d33a97f12d847108bbe6361538d1d9
jorlyf <jorlyfchannel@gmail.com>
2022-08-28 17:33:29 +0400

    react webpack app init

---
+++ app/src/entities/local/User.ts
@@ -0,0 +1,4 @@
+export default interface User {
+  login: string;
+  avatarUrl: string | null;
+}
---
+++ app/src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const element = document.getElementById("root");
+if (!element) {
+  throw new Error("root element not found");
+}
+
+const root = createRoot(element);
+root.render(
+  <App />
+);
